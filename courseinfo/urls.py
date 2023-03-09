b45c3682a702a5eca5b9e1d27584c7e0d4942cac
Meg Wu <megwu1129@gmail.com>
2023-03-08 23:29:15 -0600

    completed refactor for all views

--- courseinfo/urls.py
+++ courseinfo/urls.py
@@ -15,19 +15,31 @@ Including another URLconf
 """
 from django.urls import path
 from courseinfo.views import (
-    instructor_list_view,
-    section_list_view,
-    course_list_view,
-    semester_list_view,
-    student_list_view,
-    registration_list_view
+    InstructorList,
+    SectionList,
+    CourseList,
+    SemesterList,
+    StudentList,
+    RegistrationList,
 )
 
 urlpatterns = [
-    path('instructor/', instructor_list_view),
-    path('section/', section_list_view),
-    path('course/', course_list_view),
-    path('semester/', semester_list_view),
-    path('student/', student_list_view),
-    path('registration/', registration_list_view),
+    path('instructor/',
+         InstructorList.as_view(),
+         name='courseinfo_instructor_list_urlpattern'),
+    path('section/',
+         SectionList.as_view(),
+         name='courseinfo_section_list_urlpattern'),
+    path('course/',
+         CourseList.as_view(),
+         name='courseinfo_course_list_urlpattern'),
+    path('semester/',
+         SemesterList.as_view(),
+         name='courseinfo_semester_list_urlpattern'),
+    path('student/',
+         StudentList.as_view(),
+         name='courseinfo_student_list_urlpattern'),
+    path('registration/',
+         RegistrationList.as_view(),
+         name='courseinfo_registration_list_urlpattern'),
 ]
--- courseinfo/views.py
+++ courseinfo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 
 from courseinfo.models import (
     Instructor,
@@ -9,38 +10,54 @@ from courseinfo.models import (
     Registration
 )
 
+class InstructorList(View):
+    def get(self, request):
+        return render(
+            request,
+            'courseinfo/instructor_list.html',
+            {'instructor_list': Instructor.objects.all()}
+        )
 
-def instructor_list_view(request):
-    instructor_list = Instructor.objects.all()
-    # instructor_list = Instructor.objects.none()
-    return render(request, 'courseinfo/instructor_list.html', {'instructor_list': instructor_list})
-
-
-def section_list_view(request):
-    section_list = Section.objects.all()
-    # section_list = Section.objects.none()
-    return render(request, 'courseinfo/section_list.html', {'section_list': section_list})
-
-
-def course_list_view(request):
-    course_list = Course.objects.all()
-    # course_list = Course.objects.none()
-    return render(request, 'courseinfo/course_list.html', {'course_list': course_list})
+class SectionList(View):
+    def get(self, request):
+        return render(
+            request,
+            'courseinfo/section_list.html',
+            {'section_list': Section.objects.all()}
+        )
 
 
-def semester_list_view(request):
-    semester_list = Semester.objects.all()
-    # semester_list = Semester.objects.none()
-    return render(request, 'courseinfo/semester_list.html', {'semester_list': semester_list})
+class CourseList(View):
+    def get(self, request):
+        return render(
+            request,
+            'courseinfo/course_list.html',
+            {'course_list': Course.objects.all()}
+        )
 
 
-def student_list_view(request):
-    student_list = Student.objects.all()
-    # student_list = Student.objects.none()
-    return render(request, 'courseinfo/student_list.html', {'student_list': student_list})
+class SemesterList(View):
+    def get(self, request):
+        return render(
+            request,
+            'courseinfo/semester_list.html',
+            {'semester_list': Semester.objects.all()}
+        )
 
 
-def registration_list_view(request):
-    # registration_list = Registration.objects.all()
-    registration_list = Registration.objects.none()
-    return render(request, 'courseinfo/registration_list.html', {'registration_list': registration_list})
+class StudentList(View):
+    def get(self, request):
+        return render(
+            request,
+            'courseinfo/student_list.html',
+            {'student_list': Student.objects.all()}
+        )
+
+
+class RegistrationList(View):
+    def get(self, request):
+        return render(
+            request,
+            'courseinfo/registration_list.html',
+            {'student_list': Registration.objects.all()}
+        )
