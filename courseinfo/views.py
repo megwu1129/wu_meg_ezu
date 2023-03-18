0a14473156beeafccbc956ed875dcec431b2fbca
Meg Wu <megwu1129@gmail.com>
2023-03-17 22:10:41 -0500

    added links for each page

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from courseinfo.models import (
@@ -7,8 +7,9 @@ from courseinfo.models import (
     Course,
     Semester,
     Student,
-    Registration
+    Registration,
 )
+
 
 class InstructorList(View):
     def get(self, request):
@@ -18,12 +19,49 @@ class InstructorList(View):
             {'instructor_list': Instructor.objects.all()}
         )
 
+
+class InstructorDetail(View):
+    def get(self, request, pk):
+        instructor = get_object_or_404(
+            Instructor,
+            pk=pk
+        )
+        section_list = instructor.sections.all()
+        return render(
+            request,
+            'courseinfo/instructor_detail.html',
+            {'instructor': instructor, 'section_list': section_list}
+        )
+
+
 class SectionList(View):
     def get(self, request):
         return render(
             request,
             'courseinfo/section_list.html',
             {'section_list': Section.objects.all()}
+        )
+
+
+class SectionDetail(View):
+    def get(self, request, pk):
+        section = get_object_or_404(
+            Section,
+            pk=pk
+        )
+        semester = section.semester
+        course = section.course
+        instructor = section.instructor
+        registration_list = section.registrations.all()
+        return render(
+            request,
+            'courseinfo/section_detail.html',
+            {'section': section,
+             'semester': semester,
+             'course': course,
+             'instructor': instructor,
+             'registration_list': registration_list
+             }
         )
 
 
@@ -36,12 +74,41 @@ class CourseList(View):
         )
 
 
+class CourseDetail(View):
+    def get(self, request, pk):
+        course = get_object_or_404(
+            Course,
+            pk=pk
+        )
+        section_list = course.sections.all()
+        return render(
+            request,
+            'courseinfo/course_detail.html',
+            {'course': course,
+             'section_list': section_list}
+        )
+
+
 class SemesterList(View):
     def get(self, request):
         return render(
             request,
             'courseinfo/semester_list.html',
             {'semester_list': Semester.objects.all()}
+        )
+
+
+class SemesterDetail(View):
+    def get(self, request, pk):
+        semester = get_object_or_404(
+            Semester,
+            pk=pk
+        )
+        section_list = semester.sections.all()
+        return render(
+            request,
+            'courseinfo/semester_detail.html',
+            {'semester': semester, 'section_list': section_list}
         )
 
 
@@ -54,10 +121,38 @@ class StudentList(View):
         )
 
 
+class StudentDetail(View):
+    def get(self, request, pk):
+        student = get_object_or_404(
+            Student,
+            pk=pk
+        )
+        registration_list = student.registrations.all()
+        return render(
+            request,
+            'courseinfo/student_detail.html',
+            {'student': student,
+             'registration_list': registration_list}
+        )
+
+
 class RegistrationList(View):
     def get(self, request):
         return render(
             request,
             'courseinfo/registration_list.html',
-            {'student_list': Registration.objects.all()}
+            {'registration_list': Registration.objects.all()}
+        )
+
+
+class RegistrationDetail(View):
+    def get(self, request, pk):
+        registration = get_object_or_404(
+            Registration,
+            pk=pk
+        )
+        return render(
+            request,
+            'courseinfo/registration_detail.html',
+            {'registration': registration}
         )
