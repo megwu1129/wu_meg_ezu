1e5a664ce91e88c47f18977c923886a5331870f5
Meg Wu <megwu1129@gmail.com>
2023-04-04 14:30:37 -0500

    run migrations that load data for pagination testing and add pagination to instructor list and student list

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -10,15 +10,52 @@ from courseinfo.models import (
     Student,
     Registration,
 )
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 class InstructorList(View):
+    page_kwarg = 'page'
+    paginate_by = 25;  # 25 instructors per page
+    template_name = 'courseinfo/instructor_list.html'
+
     def get(self, request):
-        return render(
-            request,
-            'courseinfo/instructor_list.html',
-            {'instructor_list': Instructor.objects.all()}
+        instructors = Instructor.objects.all()
+        paginator = Paginator(
+            instructors,
+            self.paginate_by
         )
+        page_number = request.GET.get(
+            self.page_kwarg
+        )
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.previous_page_number())
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.next_page_number())
+        else:
+            next_url = None
+        context = {
+            'is_paginated':
+                page.has_other_pages(),
+            'next_page_url': next_url,
+            'paginator': paginator,
+            'previous_page_url': prev_url,
+            'instructor_list': page,
+        }
+        return render(
+            request, self.template_name, context)
 
 
 class InstructorDetail(View):
@@ -440,13 +477,57 @@ class SemesterDelete(View):
         return redirect('courseinfo_semester_list_urlpattern')
 
 
+# class StudentList(View):
+#     def get(self, request):
+#         return render(
+#             request,
+#             'courseinfo/student_list.html',
+#             {'student_list': Student.objects.all()}
+#         )
+
 class StudentList(View):
+    page_kwarg = 'page'
+    paginate_by = 25;  # 25 student per page
+    template_name = 'courseinfo/instructor_list.html'
+
     def get(self, request):
-        return render(
-            request,
-            'courseinfo/student_list.html',
-            {'student_list': Student.objects.all()}
+        student = Student.objects.all()
+        paginator = Paginator(
+            student,
+            self.paginate_by
         )
+        page_number = request.GET.get(
+            self.page_kwarg
+        )
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.previous_page_number())
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.next_page_number())
+        else:
+            next_url = None
+        context = {
+            'is_paginated':
+                page.has_other_pages(),
+            'next_page_url': next_url,
+            'paginator': paginator,
+            'previous_page_url': prev_url,
+            'instructor_list': page,
+        }
+        return render(
+            request, self.template_name, context)
 
 
 class StudentDetail(View):
