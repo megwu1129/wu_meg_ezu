d744dbbd77a13ae1f94dadcdfc1e9a01581a3264
Meg Wu <megwu1129@gmail.com>
2023-04-11 15:02:27 -0500

    refactor rest of the delete view

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -117,37 +117,6 @@ class SectionDelete(DeleteView):
             )
 
 
-class SectionDelete(View):
-
-    def get(self, request, pk):
-        section = self.get_object(pk)
-        registrations = section.registrations.all()
-        if registrations.count() > 0:
-            return render(
-                request,
-                'courseinfo/section_refuse_delete.html',
-                {'section': section,
-                 'registrations': registrations,
-                 }
-            )
-        else:
-            return render(
-                request,
-                'courseinfo/section_confirm_delete.html',
-                {'section': section}
-            )
-
-    def get_object(self, pk):
-        return get_object_or_404(
-            Section,
-            pk=pk)
-
-    def post(self, request, pk):
-        section = self.get_object(pk)
-        section.delete()
-        return redirect('courseinfo_section_list_urlpattern')
-
-
 class CourseList(ListView):
     model = Course
 
@@ -173,10 +142,12 @@ class CourseUpdate(UpdateView):
     template_name = 'courseinfo/course_form_update.html'
 
 
-class CourseDelete(View):
+class CourseDelete(DeleteView):
+    model = Course
+    success_url = reverse_lazy('courseinfo_course_list_urlpattern')
 
     def get(self, request, pk):
-        course = self.get_object(pk)
+        course = get_object_or_404(Course, pk=pk)
         sections = course.sections.all()
         if sections.count() > 0:
             return render(
@@ -192,16 +163,6 @@ class CourseDelete(View):
                 'courseinfo/course_confirm_delete.html',
                 {'course': course}
             )
-
-    def get_object(self, pk):
-        return get_object_or_404(
-            Course,
-            pk=pk)
-
-    def post(self, request, pk):
-        course = self.get_object(pk)
-        course.delete()
-        return redirect('courseinfo_course_list_urlpattern')
 
 
 class SemesterList(ListView):
@@ -229,10 +190,12 @@ class SemesterUpdate(UpdateView):
     template_name = 'courseinfo/semester_form_update.html'
 
 
-class SemesterDelete(View):
+class SemesterDelete(DeleteView):
+    model = Semester
+    success_url = reverse_lazy('courseinfo_semester_list_urlpattern')
 
     def get(self, request, pk):
-        semester = self.get_object(pk)
+        semester = get_object_or_404(Semester, pk=pk)
         sections = semester.sections.all()
         if sections.count() > 0:
             return render(
@@ -248,16 +211,6 @@ class SemesterDelete(View):
                 'courseinfo/semester_confirm_delete.html',
                 {'semester': semester}
             )
-
-    def get_object(self, pk):
-        return get_object_or_404(
-            Semester,
-            pk=pk)
-
-    def post(self, request, pk):
-        semester = self.get_object(pk)
-        semester.delete()
-        return redirect('courseinfo_semester_list_urlpattern')
 
 
 class StudentList(PageLinksMixin, ListView):
@@ -286,10 +239,12 @@ class StudentUpdate(UpdateView):
     template_name = 'courseinfo/student_form_update.html'
 
 
-class StudentDelete(View):
+class StudentDelete(DeleteView):
+    model = Student
+    success_url = reverse_lazy('courseinfo_student_list_urlpattern')
 
     def get(self, request, pk):
-        student = self.get_object(pk)
+        student = get_object_or_404(Student, pk=pk)
         registrations = student.registrations.all()
         if registrations.count() > 0:
             return render(
@@ -305,16 +260,6 @@ class StudentDelete(View):
                 'courseinfo/student_confirm_delete.html',
                 {'student': student}
             )
-
-    def get_object(self, pk):
-        return get_object_or_404(
-            Student,
-            pk=pk)
-
-    def post(self, request, pk):
-        student = self.get_object(pk)
-        student.delete()
-        return redirect('courseinfo_student_list_urlpattern')
 
 
 class RegistrationList(ListView):
