61421e90a5003ef75af7e0415ed0ec8eb1b58f89
Meg Wu <megwu1129@gmail.com>
2023-04-18 11:18:28 -0500

    added login url and session expire

--- wu_meg_ezu/settings.py
+++ wu_meg_ezu/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +123,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 LOGOUT_REDIRECT_URL = 'login_urlpattern'
+LOGIN_URL = reverse_lazy('login_urlpattern')
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
